3f3da2805af17d3a7d4f78e7d9e15b1e8dee4af4
stakahashi1225 <37132302+stakahashi1225@users.noreply.github.com>
2018-08-29 16:44:00 -0700

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,35 @@
+public class Arithmetic
+{
+	public static void main (String args[])
+	{
+		int a = 2;
+		int b = 4;
+		float x = 6;
+		float y = 8;
+		float tot1 = a + b;
+		float tot2 = a + x;
+		float tot3 = a + y;
+		float tot4 = b + x;
+		float tot5 = b + y;
+
+		float divtot1 = a / b;
+		float divtot2 = a / x;
+		float divtot3 = a / y;
+		float divtot4 = b / x;
+		float divtot5 = b / y;
+		System.out.println(tot1);
+		System.out.println(tot2);
+		System.out.println(tot3);
+		System.out.println(tot4);
+		System.out.println(tot5);
+
+		System.out.println(divtot1);
+		System.out.println(divtot2);
+		System.out.println(divtot3);
+		System.out.println(divtot4);
+		System.out.println(divtot5);
+
+
+
+	}
+}
